49688b753bfd4bc1287830f7baa10c2d6a13d484
Walker <azimjonxolmirzayev30@gmail.com>
2023-10-12 22:19:55 +0500

    Ma'lumotlarni olish va update qilish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sql"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sql"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/sql/MainActivity.kt
+++ app/src/main/java/com/example/sql/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sql
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,32 @@ class MainActivity : AppCompatActivity() {
 
 
 //        table yaratamiz bu tekshiradi agar bo'lsa ochib beradi bo'lmasa yaratadi
-        database.execSQL("create table if not exists people (name texts,age integer) ")
+//        database.execSQL("create table if not exists people (name texts,age integer) ")
 //        ma'lumot kiritamiz bu ma'lumotni kirgazadi va qayta qayta run qilinganda u ko'payib bora veradi
-        database.execSQL("insert into people(name,age) values('Ali',30)")
+//        database.execSQL("insert into people(name,age) values('Ali',30)")
 
+//        ma'lumotni update qilish pastdagi kodlarni yozib sinagandan so'ng
+        database.execSQL("update people set age = 40 where name = 'Ali'")
+
+
+//        cursor orqali tanlab olamiz
+        val cursor = database.rawQuery("select * from people", null)
+//        birinchi qatorga olib borish
+        cursor.moveToFirst()
+//        name va age ning indexsini topib beradi
+        val nameIndex = cursor.getColumnIndex("name")
+        val ageIndex = cursor.getColumnIndex("age")
+        do {
+//            name va age ni olib olamiz
+            val name = cursor.getString(nameIndex)
+            val age = cursor.getInt(ageIndex)
+//            logd ga chaqiramiz
+            Log.d("TAG", "name : $name , age : $age ")
+//            keyingi qatorga surulguncha ishlasin
+        } while (cursor.moveToNext())
+
+//        bazani bekitamiz
+        cursor.close()
+        database.close()
     }
 }
